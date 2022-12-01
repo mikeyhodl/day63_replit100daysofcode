8f0d286816dd5325d5b4092fea5174dda2ccf5f6
mikey <mike.owino40@yahoo.com>
2022-12-01 11:27:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# import test as bart
+# print("Countdown")
+# bart.countdown()
+
+
+# import test as tt
+
+# print("Countdown")
+# tt.countdown()
+
+
+
+import coolSubroutines as cs
+
+cs.newPrint("red")
+
+print("This should be red")
---
+++ test.py
@@ -0,0 +1,16 @@
+# for i in range(10):
+#   print(i+1)
+
+
+# def countdown():
+#   for i in range(10):
+#     print(i+1)
+
+
+import random
+
+num = random.randint(10,100) # Need the random. to refer to the random library
+
+def countdown():
+  for i in range(num):
+    print(i+1)
